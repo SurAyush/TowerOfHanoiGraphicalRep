08b3fa0329b79be8290073db1e8d25a97e09e473
SurAyush <ayushsur26@gmail.com>
2023-11-07 19:53:14 +0530

    Adding necessary changes

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ let inp = document.querySelector("#inp");
 let source = document.querySelector(".source");
 let aux = document.querySelector(".aux");
 let dest = document.querySelector(".dest");
+let btn2 = document.querySelector(".btn2");
+let s=[],a=[], d=[];
+let temp;
+let started=false;
 btn.addEventListener("click",()=>{
+    if(started == false)
+    {   
+        started = true;
     let wd= 300;
     let n=inp.value;
     let bt = 250;
@@ -23,10 +30,24 @@ btn.addEventListener("click",()=>{
         lt+=12.5;
         bt = bt - 20;
         div.id="d"+(n-i);
+        s[i]=1;
     }
     toh(n,'A','B','C');
+    }
 });
 
+btn2.addEventListener("click",()=>{
+    console.log("reset");
+    let deldiv = document.querySelectorAll(".block");
+    for(el of deldiv)
+        el.remove();
+    started = false;
+    s=[];
+    a=[];
+    d=[];
+    inp.value=0;
+
+});
 async function freeze()
 {
     return new Promise((res,rej)=>{
@@ -36,7 +57,6 @@ async function freeze()
         },1000);
     });
 }
-
 async function toh(n,start,mid,end)
 {
     if(n==0)
@@ -46,11 +66,33 @@ async function toh(n,start,mid,end)
     console.log(`Move Disk ${n} from ${start} to ${end}`);
     let x = document.querySelector(`#d${n}`);
     if(end=='C')
+    {
         dest.append(x);  // to end
+        temp = 250 - (20*d.length);
+        x.style.top = temp+"px";
+        d.push(1);
+    }
     else if(end =='B')
+    {
         aux.append(x); // to aux
+        temp = 250 - (20*a.length);
+        x.style.top = temp+"px";
+        a.push(1);
+    }
     else
+    {
         source.append(x);   // to start
+        temp = 250 - (20*s.length);
+        x.style.top = temp+"px";
+        s.push(1);
+    }
+    if(start == 'A')
+        s.pop();
+    else if(start=='B')
+        a.pop();
+    else
+        d.pop();
+
     await toh(n-1,mid,start,end);
 }
 
